33de6bf47011cb1d14d3bc1ccd3f5e644b30d774
samuel ip <samuel.c.ip@student.uts.edu.au>
2022-08-30 20:04:08 +1000

    80% complete
    Input fire changes y position

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -16,11 +16,23 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetKeyDown(KeyCode.W))
         {
             transArray[0].transform.Rotate(0.0f, 0.0f, 45.0f);
             transArray[1].transform.Rotate(0.0f, 0.0f, -45.0f);
         }
 
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Vector3 YRed = transArray[0].position;
+            Vector3 YBlue = transArray[1].position;
+            Vector3 Temp = YRed;
+            YRed.y = YBlue.y;
+            YBlue.y = Temp.y;
+            transArray[0].position = YRed;
+            transArray[1].position = YBlue;
+
+        }
     }
 }
